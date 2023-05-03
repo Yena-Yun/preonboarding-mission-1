8894ea0249f1fe539f0ebc07ccada1aea2d9c9a3
Yena-Yun <cheryleduart@gmail.com>
2023-05-03 16:15:07 +0900

    Feat: 검색어가 없을 시 '검색어 없음' 표출

--- src/components/SearchBar/ResultList/index.tsx
+++ src/components/SearchBar/ResultList/index.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { ResultItem } from './ResultItem';
 import { useAppSelector } from 'store/hooks/root';
 import { selectResult } from 'store/hooks/fetchResultThunk';
+import { ResultState } from 'types/searchResult';
 
 export const ResultList = () => {
   const { results } = useAppSelector(selectResult);
@@ -9,7 +10,8 @@ export const ResultList = () => {
   return (
     <Container>
       <SubTitle>추천 검색어</SubTitle>
-      {results.map(({ id, name }) => (
+      {results.length < 1 && <NoResultGuide>검색어 없음</NoResultGuide>}
+      {results.map(({ id, name }: ResultState) => (
         <ResultItem key={id} name={name} />
       ))}
     </Container>
@@ -25,7 +27,10 @@ const Container = styled.div`
   overflow: hidden;
 `;
 
-const SubTitle = styled.span`
+const SubTitle = styled.p`
+  margin-bottom: 0.5rem;
   font-size: 0.8rem;
   color: #849aad;
 `;
+
+const NoResultGuide = styled.p``;
